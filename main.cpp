c6270a1295a55653196c2d097f4a8a32543af413
nicolascabrera909 <nicolascabrera909@hotmail.com>
2020-07-17 10:27:13 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -100,9 +100,26 @@
 
 int main ()
 {
-    Partidas partidasJugadas;
+   Partidas partidasJugadas;
+    Make(partidasJugadas);
     jugadores inscriptos;
+    Make(inscriptos);
     torneo elTorne;
+    Crear(elTorne);
+
+
+    // si queres q este vacio comenta este bloque
+    jugador j1,j2,j3,j4;
+    crearJugador(j1,1,1,"nicolas","prueba","mvd");
+    crearJugador(j2,2,2,"juan","prueba","mvd");
+    crearJugador(j3,3,3,"emi","prueba","mvd");
+    crearJugador(j4,4,4,"tomas","prueba","mvd");
+    Insert(inscriptos,j1);
+    Insert(inscriptos,j2);
+    Insert(inscriptos,j3);
+    Insert(inscriptos,j4);
+
+
     if(!partidasLlena(partidasJugadas))
     {
         int ci1,ci2;
@@ -113,52 +130,69 @@ int main ()
         fflush(stdin);
         scanf("%d",&ci2);
         int contadorExist=0;
-        if (Member(inscriptos, ci1))
-            contadorExist++;
-        if(Member(inscriptos, ci2))
-            contadorExist=contadorExist+2;
 
-        switch (contadorExist)
+        if(hayJugadores(inscriptos))
         {
-        case 1:
-            printf ("\nEl documento ingresado para el jugador2 no existe: ");
-            break;
+            if (Member(inscriptos, ci1))
+                contadorExist++;
+            if(Member(inscriptos, ci2))
+                contadorExist=contadorExist+2;
 
-        case 2:
-            printf ("\nEl documento ingresado para el jugador1 no existe: ");
-            break;
-
-        case 3:
-            if(ci1==ci2)
-                printf ("\nLos documentos son iguales, ingrese dos documentos diferentes");
-            else
+            switch (contadorExist)
             {
-                if(JugaronPartido(elTorne,ci1,ci2))
-                    printf ("\nLos jugadores ya jugaron una partida entre si");
+
+            case 0:
+                printf ("\nLos documentos ingresados son incorrectos ");
+                break;
+
+            case 1:
+                printf ("\nEl documento ingresado para el jugador2 no existe ");
+                break;
+
+            case 2:
+                printf ("\nEl documento ingresado para el jugador1 no existe ");
+                break;
+
+
+            case 3:
+                if(ci1==ci2)
+                    printf ("\nLos documentos son iguales, ingrese dos documentos diferentes");
                 else
                 {
-                    int proximaPartida=obtengoTope(partidasJugadas);
-                    partida nuevaPartida;
-                    crearPartida(nuevaPartida, proximaPartida, ci1, ci2);
-                    InsBack (partidasJugadas, nuevaPartida);
-                    int id1,id2;
-                    id1=obtengoIdentificador(Find(inscriptos,ci1));
-                    id2=obtengoIdentificador(Find(inscriptos,ci2));
-                    InsertarArista(elTorne, id1, id2);
+                    if(JugaronPartido(elTorne,ci1,ci2))
+                        printf ("\nLos jugadores ya jugaron una partida entre si");
+                    else
+                    {
+                        int proximaPartida=obtengoTope(partidasJugadas);
+                        partida nuevaPartida;
+                        crearPartida(nuevaPartida, proximaPartida, ci1, ci2);
+                        InsBack (partidasJugadas, nuevaPartida);
+                        int id1,id2;
+                        id1=obtengoIdentificador(Find(inscriptos,ci1));
+                        id2=obtengoIdentificador(Find(inscriptos,ci2));
+                        InsertarArista(elTorne, id1, id2);
+                    }
                 }
-            }
-            break;
+                break;
 
-        default:
-            printf ("\nError");
-            break;
+            default:
+                printf ("\nError");
+                break;
+            }
         }
+        else
+            printf ("\nNo existen jugadores inscriptos");
+
+
     }
     else
         printf ("\nNo se puede crear mas partidas, el torneo esta por finalizar");
 
-return 0;
 
+
+
+        muestroTodasPartidas(partidasJugadas);
+    return 0 ;
 }
 
 
