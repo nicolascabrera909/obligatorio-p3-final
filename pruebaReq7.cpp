812462a077b655eae742b49cb80642d31d6e5d37
Emiliano Correa <e.correa95@hotmail.com>
2020-07-17 16:15:05 -0300

    va
    update jugadorGanador en jugadores
    update pruebaReq7, falta probar

--- pruebaReq7.cpp
+++ pruebaReq7.cpp
@@ -1,24 +1,29 @@
-/*
+
 //Determinar si el torneo ya ha concluido o no. Es decir, si ya han jugado todos contra todos.
 //En caso afirmativo, emitir por pantalla los datos del ganador (es decir, aquel jugador que ha ganado la mayor cantidad de partidas).
-
+/*
 #include "torneo.h"
 #include "partidas.h"
 #include "jugadores.h"
 
 void pruebaReq7 ()
 {
-    Variable diagonal booleana en true
-    Variable completa booleana en true
-    Mientas en grafo mientras diagonal y completo sean true
-        si identificadores son iguales
-            si valor  es cero
-                diagonal false
-            sino
-                si valor es cero
-                    diagonal false
-    fin
-    Si diagonal y completo son false
-        El torneo no ha terminado, todavía quedan partidas por jugar
+    if (torneoFinalizado)
+    {
+        jugador ganador = jugadorGanador(j, maximo, a1);
+        printf("El jugador ganador es: ");
+        mostrarJugador(ganador);
+    }
+    else
+    {
+        printf("El torneo no ha finalizado, todavia quedan partidas por jugar");
+    }
+
+
+    Si torneo finalizo
+        Mostrar los datos del jugador ganador
+    Sino
+        El torneo no ha finalizado, todavia quedan partidas por jugar
 }
+
 */
